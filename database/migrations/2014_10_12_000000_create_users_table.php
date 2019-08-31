4e8298337b753209ba4e9b233c92d45168e1738d
zachkhon <54166101+zachkhon@users.noreply.github.com>
2019-09-01 01:57:36 +0700

    Add files via upload

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone', 255)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->date('dob')->nullable();
+            $table->string('avatar', 255)->nullable();
+            $table->unsignedTinyInteger('privilege');
+                // 0: inactive
+                // 1: user
+                // 100: admin
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
