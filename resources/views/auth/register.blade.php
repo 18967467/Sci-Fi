c9c9e360aa4a1dab9b9f7133cf05220d092a416a
18967467 <54165910+18967467@users.noreply.github.com>
2019-09-02 22:53:03 +1000

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -38,7 +40,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -60,7 +62,35 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
+                           <div class="form-group row">
+  	                       <div class="col-md-4">
+  		                   <img width="150" height="200" src="{{ asset('img/bg-avatar.png') }}" alt=""></a>
+  	                       </div>
+  	                       <div class="col-md-6 ">
+    	                   <input id="Upload photo" name="Upload photo" class="input-file" type="file">
+                           </div>
+                           </div>
+                           <div class="form-group row">
+  		                    <label class="col-md-4 control-label" for="dob">Date of birth</label>
+                         	<div class="col-md-6">        
+       	                	<input id="dob" name="dob" type="text" placeholder="dd/mm/yyyy" class="form-control input-md">
+      	                   </div>
+  	                       </div>
 
+                           <div class="form-group row">
+  		                    <label class="col-md-4 control-label" for="phone">Phone</label>
+       	                    <div class="col-md-6">        
+       		                <input id="phone" name="phone" type="text" placeholder="04xx xxx xxx" class="form-control input-md">
+      	                    </div>
+  	                        </div>
+							   <div class="form-group row">
+  		                       <label class="col-md-4 control-label" for="address">Address</label>
+       	                       <div class="col-md-6">        
+       		                   <input id="address" name="address" type="text" placeholder="1b street..." class="form-control input-md">
+      	                        </div>
+  	                            </div>
+
+							
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -73,5 +103,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> 
 @endsection
