41d4059c14cddb9eddd438ac3f470d31863e19db
18967467 <54165910+18967467@users.noreply.github.com>
2019-09-02 22:49:54 +1000

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,14 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 255)->nullable();
+            $table->string('address', 255)->nullable();
+            $table->date('dob')->nullable();
+            $table->unsignedTinyInteger('privilege');
+                // 0: inactive
+                // 1: user
+                // 100: admin
+			$table->mediumText('image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
