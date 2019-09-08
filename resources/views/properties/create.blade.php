ffc86f22b174aa225a81838b340a12d677f4975e
18967467 <54165910+18967467@users.noreply.github.com>
2019-09-09 08:02:47 +1000

    Add files via upload

---
+++ resources/views/properties/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<div class="container">
+@if ($errors->any())
+<div class="alert alert-danger">
+<ul>
+@foreach($errors->all() as $error)
+<li>{{$error}}</li>
+@endforeach
+</ul>
+</div><br />
+@endif
+<div class="row">
+<form method="post" action="{{route('properties.store')}}">
+<div class="form-group">
+<input type="hidden" value="{{csrf_token()}}" name="_token" />
+<label for="name">Name of Property</label>
+<input type="text" class="form-control" name="name"/>
+</div>
+<div class="form-group">
+<label for="input_type_id">Input Type</label>
+<select class="form-control" name="input_type_id" id="input_type_id">
+<option value="1" selected>Text</option>
+<option value="2">Checkbox</option>
+<option value="3">Radio</option>
+<option value="4">Date</option>
+<option value="5">File</option>
+</select>
+</div>
+<div class="form-group">
+<label for="description">Description</label>
+<textarea cols="5" rows="5" class="form-control" name="description"></textarea>
+</div>
+<label for="Order">Order</label>
+<input type="text" class="form-control" name="order"/>
+</div>
+<button type="submit" class="btn btn-primary">Create</button>
+</form>
+</div>
+</div>
+@endsection('content')
