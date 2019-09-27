0db74a8c365e8283d14dc384f9b564640b00a2fa
18967467 <54165910+18967467@users.noreply.github.com>
2019-09-27 22:35:04 +1000

    Add files via upload

---
+++ app/Models/Robot.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Robot extends Model
+{
+    
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'robots';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+                  'user_id',
+                  'state'
+              ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+    
+    /**
+     * Get the User for this model.
+     *
+     * @return App\Models\User
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the comment for this model.
+     *
+     * @return App\Models\Comment
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Get the report for this model.
+     *
+     * @return App\Models\Report
+     */
+    public function reports()
+    {
+        return $this->hasMany('App\Models\Report','robot_id','id');
+    }
+
+    /**
+     * Get the robotInfo for this model.
+     *
+     * @return App\Models\RobotInfo
+     */
+    public function robotInfos()
+    {
+        return $this->hasMany('App\Models\RobotInfo','robot_id','id');
+    }
+
+    /**
+     * Get the savedList for this model.
+     *
+     * @return App\Models\SavedList
+     */
+    public function savedList()
+    {
+        return $this->hasMany('App\Models\SavedList','robot_id','id');
+    }
+
+
+    /**
+     * Get created_at in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return \DateTime::createFromFormat($this->getDateFormat(), $value)->format('j/n/Y g:i A');
+    }
+
+    /**
+     * Get updated_at in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getUpdatedAtAttribute($value)
+    {
+        return \DateTime::createFromFormat($this->getDateFormat(), $value)->format('j/n/Y g:i A');
+    }
+
+}
---
+++ app/Models/User.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+    * The database primary key value.
+    *
+    * @var string
+    */
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+                  'name',
+                  'email',
+                  'email_verified_at',
+                  'password',
+                  'phone',
+                  'address',
+                  'dob',
+                  'avatar',
+                  'privilege',
+                  'remember_token'
+              ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
+    
+    /**
+     * Get the comment for this model.
+     *
+     * @return App\Models\Comment
+     */
+    public function comments()
+    {
+        return $this->hasMany('App\Models\Comment');
+    }
+
+    /**
+     * Get the mail for this model.
+     *
+     * @return App\Models\Mail
+     */
+    public function mail()
+    {
+        return $this->hasOne('App\Models\Mail','user_id','id');
+    }
+
+    /**
+     * Get the report for this model.
+     *
+     * @return App\Models\Report
+     */
+    public function report()
+    {
+        return $this->hasOne('App\Models\Report','user_id','id');
+    }
+
+    /**
+     * Get the robot for this model.
+     *
+     * @return App\Models\Robot
+     */
+    public function robots()
+    {
+        return $this->hasMany('App\Models\Robot');
+    }
+
+    /**
+     * Get the savedList for this model.
+     *
+     * @return App\Models\SavedList
+     */
+    public function savedList()
+    {
+        return $this->hasOne('App\Models\SavedList','user_id','id');
+    }
+
+    /**
+     * Set the email_verified_at.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setEmailVerifiedAtAttribute($value)
+    {
+        $this->attributes['email_verified_at'] = !empty($value) ? \DateTime::createFromFormat('j/n/Y', $value) : null;
+    }
+
+    /**
+     * Set the dob.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setDobAttribute($value)
+    {
+        $this->attributes['dob'] = !empty($value) ? \DateTime::createFromFormat('j/n/Y', $value) : null;
+    }
+
+    /**
+     * Get email_verified_at in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getEmailVerifiedAtAttribute($value)
+    {
+        return !empty($value) ? \DateTime::createFromFormat($this->getDateFormat(), $value)->format('j/n/Y') : null;
+    }
+
+    /**
+     * Get dob in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getDobAttribute($value)
+    {
+        return !empty($value) ? \DateTime::createFromFormat($this->getDateFormat1(), $value)->format('j/n/Y') : null;
+    }
+
+    /**
+     * Get created_at in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getCreatedAtAttribute($value)
+    {
+        return !empty($value) ? \DateTime::createFromFormat($this->getDateFormat(), $value)->format('j/n/Y') : null;
+        
+    }
+
+    /**
+     * Get updated_at in array format
+     *
+     * @param  string  $value
+     * @return array
+     */
+    public function getUpdatedAtAttribute($value)
+    {
+        return !empty($value) ? \DateTime::createFromFormat($this->getDateFormat(), $value)->format('j/n/Y') : null;
+    }
+    
+    public function getDateFormat1()
+    {
+        return 'Y-m-d';
+    }
+
+}
